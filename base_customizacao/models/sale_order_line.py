223a66c49a9b7340f8caefcfb26f218da28dcda9
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-08-06 10:16:41 -0300

    Adicionado total de quantidades, e indices nas linhas do pedido

--- base_customizacao/models/sale_order_line.py
+++ base_customizacao/models/sale_order_line.py
@@ -4,14 +4,39 @@ Created on 06/08/2015
 @author: danimar
 '''
 
-from openerp import models, api
+from openerp import models, api, fields
 
-class sale_order_line(models.Model):
+class SaleOrdeLine(models.Model):
     _inherit = 'sale.order.line'
+    
+    @api.multi
+    def _get_line_numbers(self):
+        index = 1
+        for line in self[0].order_id.order_line:
+            line.line_number = index
+            index += 1
+    
+    line_number = fields.Integer(string="Indice", compute='_get_line_numbers', store=True)
     
     @api.model
     def default_get(self, fields_list):
-        vals = super(sale_order_line, self).default_get(fields_list)
+        vals = super(SaleOrdeLine, self).default_get(fields_list)
         if 'ref_ids' in self.env.context.keys():
-            vals.update({'sequence': len(self.env.context['ref_ids']) + 1 })
+            vals.update({'line_number': len(self.env.context['ref_ids']) + 1 })
         return vals
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    
+    @api.multi
+    def _count_quantity(self):        
+        for order in self:
+            total = 0.0
+            for line in self[0].order_line:
+                total += line.product_uom_qty
+            order.total_items = total
+        
+    total_items = fields.Float(string="Total de quantidades",
+                               digits=(18,2),compute='_count_quantity')
+    
+    
